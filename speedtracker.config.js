3dc60b1522adf4247c08a42806d427584cfaff51
Casper Juel <info@casperjuel.dk>
2019-03-26 10:27:22 +0100

    Update speedtracker.config.js

--- speedtracker.config.js
+++ speedtracker.config.js
@@ -1,22 +1,69 @@
 const config = {
   profiles: {
-    /*
-    // Example profile
     test: {
-      name: 'BBC One',
+      name: 'Novasol.de',
       parameters: {
         connectivity: 'Cable',
-        location: 'London_EC2',
-        runs: 5,
-        url: 'https://www.bbc.co.uk/bbcone'
+        location: 'London_EC2:Chrome',
+        runs: 3,
+        block: "www.google-analytics.com",
+        url: 'https://novasol.de'
       },
-      budgets: [
+     /* budgets: [
         {
           metric: 'TTFB',
           max: 1000
         }
-      ]
-    }*/
+      ]*/
+    },
+    test: {
+      name: 'Novasol.dk',
+      parameters: {
+        connectivity: 'Cable',
+        location: 'London_EC2:Chrome',
+        runs: 3,
+        block: "www.google-analytics.com",
+        url: 'https://novasol.dk'
+      },
+     /* budgets: [
+        {
+          metric: 'TTFB',
+          max: 1000
+        }
+      ]*/
+    },
+    test: {
+      name: 'House DE',
+      parameters: {
+        connectivity: 'Cable',
+        location: 'London_EC2:Chrome',
+        runs: 3,
+        block: "www.google-analytics.com",
+        url: 'https://www.novasol.de/p/CIC638?adults=2&children=0&pets=0'
+      },
+     /* budgets: [
+        {
+          metric: 'TTFB',
+          max: 1000
+        }
+      ]*/
+    },
+    test: {
+      name: 'House DK',
+      parameters: {
+        connectivity: 'Cable',
+        location: 'London_EC2:Chrome',
+        runs: 3,
+        block: "www.google-analytics.com",
+        url: 'https://www.novasol.dk/feriehuse/kroatien/istrien/vodnjan/feriehuse-vodnjan-svkirin-cic638?adults=2&children=0&pets=0'
+      },
+     /* budgets: [
+        {
+          metric: 'TTFB',
+          max: 1000
+        }
+      ]*/
+    }
   }
 }
 
